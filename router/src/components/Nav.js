6d1f359c3bc31c648e70b3b5e11f6aabdcb5a1c6
YoonDH <dgfh0450@gamil.com>
2022-10-28 01:42:05 +0900

    nav style 해결 필요

--- router/src/components/Nav.js
+++ router/src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './css/Nav.css'
 
 function Nav(props) {
     return (
         <div>
-        <NavLink  to='/content/food'>Food</NavLink>
-        <NavLink  to='/content/influencer'>influencer</NavLink>
-        <NavLink  to='/content/contents'>contents</NavLink>
-        <NavLink  to='/content/tour'>tour</NavLink>
+        <NavLink  to='/content/food' className={({ isActive }) => isActive ? 'nav_active' : 'nav_inactive'} id='Food'>Food</NavLink>
+        <NavLink  to='/content/influencer' className={({ isActive }) => isActive ? 'active' : 'nav_inactive'} id='Influencer'>influencer</NavLink>
+        <NavLink  to='/content/contents' className={({ isActive }) => isActive ? 'active' : 'nav_inactive'} id='Contents'>contents</NavLink>
+        <NavLink  to='/content/tour' className={({ isActive }) => isActive ? 'active' : 'nav_inactive'} id='Tour'>tour</NavLink>
         </div>
     );
 }
