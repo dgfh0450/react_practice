5e4173bf081eb2b97cfd6dd544c58c072365c549
YoonDH <dgfh0450@gamil.com>
2022-10-28 00:41:29 +0900

    message

--- router/src/components/Food.js
+++ router/src/components/Food.js
@@ -7,7 +7,7 @@ function Food(props) {
         <div>
             <NavLink  to='/content/food/kimchi' className={({ isActive }) => isActive ? 'active' : 'inactive'} id='kimchi'></NavLink>
             <NavLink  to='/content/food/bulgogi' className={({ isActive }) => isActive ? 'active' : 'inactive'} id='bulgogi'></NavLink>
-            <NavLink  to='/content/food/' className={({ isActive }) => isActive ? 'active' : 'inactive'} id='bibimbap'></NavLink>
+            <NavLink  to='/content/food/bibimbap' className={({ isActive }) => isActive ? 'active' : 'inactive'} id='bibimbap'></NavLink>
             <NavLink  to='/content/food/pork' className={({ isActive }) => isActive ? 'active' : 'inactive'} id='pork'></NavLink>
         </div>
     );
