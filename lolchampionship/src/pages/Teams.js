7a87dd8914bf11fdb4ad9add7e338e7ba3219196
YoonDH <dgfh0450@gamil.com>
2022-11-05 02:01:41 +0900

    img : src -> public

--- lolchampionship/src/pages/Teams.js
+++ lolchampionship/src/pages/Teams.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import top from '../images/top.jpg'
-import mid from '../images/mid.jpg'
-import jug from '../images/jg.jpg'
-import adc from '../images/adc.jpg'
-import spt from '../images/spt.jpg'
 
 export default function Teams() {
     let teamName= useParams();
@@ -42,11 +37,7 @@ export default function Teams() {
         {
             let img = null;
             for(let j=0; j<arr[i][1].length; j++) {
-                if(arr[i][1][j][1]=="top")  {img = top}
-                else if(arr[i][1][j][1]=="jug") {img = jug}
-                else if(arr[i][1][j][1]=="mid") {img = mid}
-                else if(arr[i][1][j][1]=="adc") {img = adc}
-                else if(arr[i][1][j][1]=="spt") {img = spt}
+                img = process.env.PUBLIC_URL+'/images/'+arr[i][1][j][1]+'.jpg';
 
                 lis.push(<li style={{ fontFamily:'RIXGOL', display:'flex', alignItems:'center'}}><img style={{width:'30px', height:'30px'}} src={img}></img> - {arr[i][1][j][0]}</li>)
             }
